8ad9d201dc9266d018e9156bbd109bf26d6eadcd
sergiomuguruza <sergiomuguruza@gmail.com>
2023-08-16 09:31:26 -0300

    16/8

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [Nombre, setNombre] = useState("");
@@ -10,7 +11,9 @@ function App() {
   const [Cargo, setCargo] = useState("");
   const [Años, setAños] = useState(0);
   const [isFormValid, setIsFormValid] = useState(false);
-  const [employeeList, setEmployeeList] = useState([]);
+  const [EmployeeList, setEmployeeList] = useState([]);
+  const [Editar, setEditar] = useState(false);
+  const [id, setId] = useState(0);
 
   const validateForm = () => {
     if (Nombre !== "" && Edad !== "" && Pais !== "" && Cargo !== "" && Años !== "") {
@@ -18,6 +21,16 @@ function App() {
     } else {
       setIsFormValid(false);
     }
+  }
+
+  const editarempleado = (val) => {
+    setEditar(true);
+    setNombre(val.Nombre);
+    setEdad(val.Edad);
+    setPais(val.Pais);
+    setCargo(val.Cargo);
+    setAños(val.Años);
+    setId(val.id);
   }
 
   useEffect(() => {
@@ -32,23 +45,52 @@ function App() {
   }, []);
 
   const add = () => {
-    axios.post("http://localhost:3001/create", {
+    const newEmployee = {
       Nombre: Nombre,
       Edad: Edad,
       Pais: Pais,
       Cargo: Cargo,
       Años: Años,
-    }).then(() => {
-      const successAlert = document.createElement('div');
-      successAlert.className = 'success-alert';
-      successAlert.innerText = 'El empleado se ha registrado correctamente';
-      document.body.appendChild(successAlert);
+    };
 
-      setTimeout(() => {
-        successAlert.remove();
-      }, 3000); // Desaparece después de 3 segundos
-    });
+    if (Editar) {
+      axios.post(`http://localhost:3001/update/${id}`, newEmployee)
+        .then(() => {
+          // Realiza una nueva llamada para obtener la lista actualizada
+          axios.get("http://localhost:3001/employees")
+            .then(response => {
+              setEmployeeList(response.data); // Actualiza la lista de empleados
+              resetForm();
+            })
+            .catch(error => {
+              console.error("Error al obtener la lista de empleados:", error);
+            });
+        });
+    } else {
+      axios.post("http://localhost:3001/create", newEmployee)
+        .then(() => {
+          // Realiza una nueva llamada para obtener la lista actualizada
+          axios.get("http://localhost:3001/employees")
+            .then(response => {
+              setEmployeeList(response.data); // Actualiza la lista de empleados
+              resetForm();
+            })
+            .catch(error => {
+              console.error("Error al obtener la lista de empleados:", error);
+            });
+        });
+    }
   }
+
+  const resetForm = () => {
+    setNombre("");
+    setEdad(0);
+    setPais("");
+    setCargo("");
+    setAños(0);
+    setEditar(false);
+    setId(0);
+  };
 
   const handleNombreChange = (event) => {
     setNombre(event.target.value);
@@ -75,54 +117,124 @@ function App() {
     validateForm();
   }
 
+  const eliminarEmpleado = (employeeId) => {
+    axios.delete(`http://localhost:3001/delete/${employeeId}`)
+      .then(() => {
+        // Actualizar la lista de empleados después de eliminar
+        const updatedEmployeeList = EmployeeList.filter((employee) => employee.id !== employeeId);
+        setEmployeeList(updatedEmployeeList);
+      })
+      .catch(error => {
+        console.error("Error al eliminar el empleado:", error);
+      });
+  }
+
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Registro de Empleados</h1>
-        <div className="form">
-          <label htmlFor="nombre">Nombre:</label>
-          <input
-            id="nombre"
-            onChange={handleNombreChange}
-            type="text"
-          />
-          <label htmlFor="edad">Edad:</label>
-          <input
-            id="edad"
-            onChange={handleEdadChange}
-            type="number"
-          />
-          <label htmlFor="pais">Pais:</label>
-          <input
-            id="pais"
-            onChange={handlePaisChange}
-            type="text"
-          />
-          <label htmlFor="cargo">Cargo:</label>
-          <input
-            id="cargo"
-            onChange={handleCargoChange}
-            type="text"
-          />
-          <label htmlFor="anos">Años:</label>
-          <input
-            id="anos"
-            onChange={handleAñosChange}
-            type="number"
-          />
-          <button onClick={add} disabled={!isFormValid}>Registrar</button>
+    <div className="container">
+      <div className="card text-center">
+        <div className="card-header">
+          <h1>Gestión de Empleados</h1>
         </div>
-        <div className="employee-list">
-          <h2>Lista de Empleados</h2>
-          <ul>
-            {employeeList.map(employee => (
-              <li key={employee.id}>
-                Nombre: {employee.Nombre}, Edad: {employee.Edad}, País: {employee.Pais}, Cargo: {employee.Cargo}, Años: {employee.Años}
-              </li>
-            ))}
-          </ul>
+        <div className="card-body">
+          <form>
+            <div className="row mb-3">
+              <div className="col">
+                <label htmlFor="nombre" className="form-label">Nombre:</label>
+                <input type="text" id="nombre" value={Nombre} onChange={handleNombreChange} className="form-control" placeholder="Ingrese un nombre" />
+              </div>
+              <div className="col">
+                <label htmlFor="edad" className="form-label">Edad:</label>
+                <input type="number" id="edad" value={Edad} onChange={handleEdadChange} className="form-control" placeholder="Ingrese su edad" />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col">
+                <label htmlFor="pais" className="form-label">País:</label>
+                <input type="text" id="pais" value={Pais} onChange={handlePaisChange} className="form-control" placeholder="Ingrese País" />
+              </div>
+              <div className="col">
+                <label htmlFor="cargo" className="form-label">Cargo:</label>
+                <input type="text" id="cargo" value={Cargo} onChange={handleCargoChange} className="form-control" placeholder="Ingrese el cargo" />
+              </div>
+            </div>
+            <div className="row mb-3">
+              <div className="col">
+                <label htmlFor="años" className="form-label">Años:</label>
+                <input type="number" id="años" value={Años} onChange={handleAñosChange} className="form-control" placeholder="Ingrese Años de antigüedad" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                {Editar ? (
+                  <div className="btn-group">
+                    <button className="btn btn-warning btn-sm mr-2" onClick={add}>
+                      Actualizar
+                    </button>
+                    <button className="btn btn-secondary btn-sm" onClick={resetForm}>
+                      Cancelar
+                    </button>
+                  </div>
+                ) : (
+                  <button className="btn btn-success" onClick={add} disabled={!isFormValid}>
+                    Registrar
+                  </button>
+                )}
+              </div>
+            </div>
+          </form>
         </div>
       </div>
+      <table className="table table-striped mt-3">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Edad</th>
+            <th scope="col">Pais</th>
+            <th scope="col">Cargo</th>
+            <th scope="col">Años</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+  {EmployeeList.map((val, key) => (
+    <tr key={val.id}>
+      <th>{val.id}</th>
+      <td>{val.Nombre}</td>
+      <td>{val.Edad}</td>
+      <td>{val.Pais}</td>
+      <td>{val.Cargo}</td>
+      <td>{val.Años}</td>
+      <td>
+        <div className="btn-group button-group" role="group" aria-label="Acciones"> {/* Agregamos la clase .button-group aquí */}
+          <button
+            type="button"
+            onClick={() => {
+              editarempleado(val);
+            }}
+            className="btn btn-danger btn-sm"
+          >
+            Editar
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              eliminarEmpleado(val.id);
+            }}
+            className="btn btn-warning btn-sm"
+          >
+            Eliminar
+          </button>
+          <button type="button" className="btn btn-success btn-sm">
+            SINUSO
+          </button>
+        </div>
+      </td>
+    </tr>
+  ))}
+</tbody>
+
+      </table>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,50 +2,77 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
-const path = require("path"); // Agrega esta línea
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "scr"))); // Agrega esta línea
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1320",
-    database: "empleados_crud"
+  host: "localhost",
+  user: "root",
+  password: "1320",
+  database: "empleados_crud",
 });
 
 app.post("/create", (req, res) => {
-    const Nombre = req.body.Nombre;
-    const Edad = req.body.Edad;
-    const Pais = req.body.Pais;
-    const Cargo = req.body.Cargo;
-    const Años = req.body.Años;
+  const { Nombre, Edad, Pais, Cargo, Años } = req.body;
 
-    db.query(
-        "INSERT INTO empleados(nombre,edad,pais,cargo,años) VALUES(?,?,?,?,?)",
-        [Nombre, Edad, Pais, Cargo, Años],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send("empleado registrado con exito");
-            }
-        }
-    );
+  db.query(
+    "INSERT INTO empleados(nombre,edad,pais,cargo,años) VALUES(?,?,?,?,?)",
+    [Nombre, Edad, Pais, Cargo, Años],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al registrar el empleado");
+      } else {
+        res.send("Empleado registrado con éxito");
+      }
+    }
+  );
 });
 
-// Endpoint para obtener la lista de empleados
-app.get('/employees', async (req, res) => {
-    try {
-      const [results] = await db.promise().query("SELECT * FROM empleados");
-      res.json(results);
-    } catch (error) {
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM empleados", (err, results) => {
+    if (err) {
       console.error("Error al obtener la lista de empleados:", error);
       res.status(500).send("Error al obtener la lista de empleados");
+    } else {
+      res.json(results);
     }
   });
+});
+
+app.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const { Nombre, Edad, Pais, Cargo, Años } = req.body;
+
+  db.query(
+    "UPDATE empleados SET nombre=?,edad=?,pais=?,cargo=?,años=? WHERE id=?",
+    [Nombre, Edad, Pais, Cargo, Años, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error al actualizar el empleado");
+      } else {
+        res.send("Empleado actualizado con éxito");
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM empleados WHERE id=?", [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error al eliminar el empleado");
+    } else {
+      res.send("Empleado eliminado con éxito");
+    }
+  });
+});
 
 app.listen(3001, () => {
-    console.log("corriendo en el puerto 3001");
+  console.log("Corriendo en el puerto 3001");
 });
